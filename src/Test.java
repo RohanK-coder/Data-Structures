32378fa02612215870efeae474dfdb49b0aea549
RohanK-coder <kyrohan7730@gmail.com>
2024-08-29 14:01:55 +0530

    Some more multithreading

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,19 @@
 import java.net.URL;
 
 public class Test {
+
     public static void main(String[] args) {
+        Thread t1 = new Thread(()->{
+            for (int i=0;i<100;i++){
+                System.out.print(i+ " ");
+            }
+        });
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println("Hello World");
         try{
             URL url = new URL("https://www.google.com");
@@ -16,6 +28,7 @@ public class Test {
         System.out.println(Math.sqrt(156));
     }
 }
+
 
 //commit testing
 //commit retest
