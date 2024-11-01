d46b7b9349027edb1e52adc4da26355cf9a789a6
RohanK-coder <kyrohan7730@gmail.com>
2024-11-01 12:04:55 +0530

    Added Selection Sort and Bubble Sort in one code.

--- src/gitScript.sh
+++ src/gitScript.sh
@@ -2,5 +2,5 @@
 echo "Commit Starting"
 
 git add .
-git commit -m "..."
+git commit -m "Added Selection Sort and Bubble Sort in one code."
 git push origin main
--- src/strivers/Sorting.java
+++ src/strivers/Sorting.java
@@ -5,13 +5,43 @@ import java.util.Arrays;
 public class Sorting {
     public static int[] arr = new int[]{4,5,2,3,1};
     public static void main(String[] args) {
+        System.out.print("Before Sorting : ");
         System.out.println(Arrays.toString(arr));
-        
+        System.out.print("After Sorting : ");
+        //selectionSort();
+        //bubbleSort();
+        System.out.println(Arrays.toString(arr));
+
 
     }
     public static void swap(int a,int b){
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
+    }
+
+    //Selection Sort Implementation
+    public static void selectionSort(){
+        int min;
+        for(int i=0;i<arr.length;i++){
+            min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            swap(i,min);
+        }
+    }
+
+    //Bubble Sort Implementation
+    public static void bubbleSort(){
+        for(int i=arr.length-1;i>=0;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(j,j+1);
+                }
+            }
+        }
     }
 }
